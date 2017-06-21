66c6076dce420817bb888fe75f03508f72fcfb66
nevstruev-dmitriy <nevstruev.dmitriy@mail.ru>
2017-06-21 15:08:24 +0300

    Новый файл НевструевВладислав

--- TelegramBotLKSH/sempel/НевструевВладислав/НевструевВладислав.cpp
+++ TelegramBotLKSH/sempel/НевструевВладислав/НевструевВладислав.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <random>
+#include <ctime>
 
 using namespace std;
 
-mt19937 rr(random_device{}());
+//mt19937 rr(random_device{}());
 
 int main() {
+	srand(time(0));
 	for (int i = 1; i <= 112; ++i) {
-		if (rr() % 10 == 2) {
+		if (rand() % 10 == 2) {
 			cout << 0 << endl;
 		}
 		else {
