7c20d60dd1c3f6ebbbc15dc57222496a0b6a1f3e
nevstruev-dmitriy <nevstruev.dmitriy@mail.ru>
2017-06-21 21:34:43 +0300

    Новый файл НевструевДмитрий

--- TelegramBotLKSH/bot.py
+++ TelegramBotLKSH/bot.py
@@ -426,34 +426,34 @@ def inputMesData(mes):
 
 	nameSol = mes.from_user.last_name + mes.from_user.first_name
 
-#	try:
-	chatId = mes.chat.id
-	fileInfo = bot.get_file(mes.document.file_id)
-	download = bot.download_file(fileInfo.file_path)
+	try:
+		chatId = mes.chat.id
+		fileInfo = bot.get_file(mes.document.file_id)
+		download = bot.download_file(fileInfo.file_path)
 
 #		print(nameSol)
 	
-	if not(nameSol in os.listdir("sempel/")):
-		os.makedirs("sempel/" + nameSol)
-	src = 'sempel/' + nameSol + "/" + nameSol + ".cpp"
+		if not(nameSol in os.listdir("sempel/")):
+			os.makedirs("sempel/" + nameSol)
+		src = 'sempel/' + nameSol + "/" + nameSol + ".cpp"
 
-	with open(src, 'wb') as new_file:
-		new_file.write(download)
+		with open(src, 'wb') as new_file:
+			new_file.write(download)
 
-	logCheck = checkSol(nameSol)
-	if logCheck != "OK":
-		bot.reply_to(mes, "Не используй %s в своём решении, пожалуйста))" %
-					 logCheck)
-	else:
-		logMake = makeName(nameSol)
-		if logMake != "OK":
-			bot.reply_to(mes, "Решение не скомпилировалось(\n log: %s\n"
-						 % logMake[:2113])
+		logCheck = checkSol(nameSol)
+		if logCheck != "OK":
+			bot.reply_to(mes, "Не используй %s в своём решении, пожалуйста))" %
+						 logCheck)
 		else:
-			bot.reply_to(mes, "Решение принятно))")
+			logMake = makeName(nameSol)
+			if logMake != "OK":
+				bot.reply_to(mes, "Решение не скомпилировалось(\n log: %s\n"
+							 % logMake[:2113])
+			else:
+				bot.reply_to(mes, "Решение принятно))")
 	
-#	except Exception as e:
-	bot.reply_to(mes, "Не получилось((")
+	except Exception as e:
+		bot.reply_to(mes, "Не получилось((")
 	
 	log = "{%s}:%s: Добавлин новый файл" % (VarTime, nameSol)
 	log += commitGit("Новый файл " + nameSol)
