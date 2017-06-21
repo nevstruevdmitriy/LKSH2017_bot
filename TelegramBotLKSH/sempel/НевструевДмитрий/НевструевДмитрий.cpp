6aa1e981e1bf20b76ac3af6d1d363b33456d87df
nevstruev-dmitriy <nevstruev.dmitriy@mail.ru>
2017-06-21 18:57:37 +0300

    Новый файл НевструевДмитрий

--- TelegramBotLKSH/sempel/НевструевДмитрий/НевструевДмитрий.cpp
+++ TelegramBotLKSH/sempel/НевструевДмитрий/НевструевДмитрий.cpp
@@ -1,32 +1,8 @@
 #include <iostream>
+
 using namespace std;
+
 int main() {
-	int k, i = 0;
-	int bal = 0;
-	int myBal = 1;
-	cout << 1 << endl;
-	cout.flush();
-	for(int i = 0; i < 200; ++i) {
-		cin >> k;
-		if (k == 1) {
-			bal++;
-		}
-		if (k == 3 || k == 2) {
-			bal = 0;
-		}
-		if (myBal == 3) {
-			cout << 3 << endl;
-			cout.flush();
-			return 0;
-		}
-		if (bal == 0 || i / myBal > 25) {
-			myBal++;
-			cout << "1" << endl;
-			cout.flush();
-		} else {
-			cout << 0 << endl;
-			cout.flush();
-		}
-	}
+	while(1);
 	return 0;
 }
