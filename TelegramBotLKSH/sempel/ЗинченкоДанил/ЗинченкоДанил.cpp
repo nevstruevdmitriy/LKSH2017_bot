55d80c6545c8af71effefa02c8830500df34c7c2
nevstruev-dmitriy <nevstruev.dmitriy@mail.ru>
2017-06-22 13:52:51 +0300

    Новый файл ЗинченкоДанил

---
+++ TelegramBotLKSH/sempel/ЗинченкоДанил/ЗинченкоДанил.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<map>
+#include<set>
+#include<math.h>
+#include<algorithm>
+#include<time.h>
+#include<stdio.h>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<fstream>
+#include<unordered_map>
+#include<bitset>
+#include<cstdio>
+
+
+using namespace std;
+
+/*
+^(0,0)^      (¬_¬)     ^(0,0)^      (¬_¬)      ^(0,0)^      (¬_¬)      ^(0,0)^      (¬_¬)      ^(0,0)^
+^(0,0)^      (¬_¬)     ^(0,0)^      (¬_¬)      ^(0,0)^      (¬_¬)      ^(0,0)^      (¬_¬)      ^(0,0)^
+^(0,0)^      (¬_¬)     ^(0,0)^      (¬_¬)      ^(0,0)^      (¬_¬)      ^(0,0)^      (¬_¬)      ^(0,0)^
+^(0,0)^      (¬_¬)     ^(0,0)^      (¬_¬)      ^(0,0)^      (¬_¬)      ^(0,0)^      (¬_¬)      ^(0,0)^
+^(0,0)^      (¬_¬)     ^(0,0)^      (¬_¬)      ^(0,0)^      (¬_¬)      ^(0,0)^      (¬_¬)      ^(0,0)^
+^(0,0)^      (¬_¬)     ^(0,0)^      (¬_¬)      ^(0,0)^      (¬_¬)      ^(0,0)^      (¬_¬)      ^(0,0)^
+^(0,0)^      (¬_¬)     ^(0,0)^      (¬_¬)      ^(0,0)^      (¬_¬)      ^(0,0)^      (¬_¬)      ^(0,0)^
+^(0,0)^      (¬_¬)     ^(0,0)^      (¬_¬)      ^(0,0)^      (¬_¬)      ^(0,0)^      (¬_¬)      ^(0,0)^
+^(0,0)^      (¬_¬)     ^(0,0)^      (¬_¬)      ^(0,0)^      (¬_¬)      ^(0,0)^      (¬_¬)      ^(0,0)^
+^(0,0)^      (¬_¬)     ^(0,0)^      (¬_¬)      ^(0,0)^      (¬_¬)      ^(0,0)^      (¬_¬)      ^(0,0)^
+^(0,0)^      (¬_¬)     ^(0,0)^      (¬_¬)      ^(0,0)^      (¬_¬)      ^(0,0)^      (¬_¬)      ^(0,0)^
+*/
+
+#define ll double
+#define pb push_back
+#define mp make_pair
+#define s() size()
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.setf(ios::fixed);
+	cout.precision(0);
+	while (true)
+	{
+		cout << 0 << endl;
+		cout << 1 << endl;
+		cout << 3 << endl;
+	}
+}
