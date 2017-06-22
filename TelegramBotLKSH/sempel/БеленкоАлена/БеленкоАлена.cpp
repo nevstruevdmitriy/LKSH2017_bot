e2b84852db87fb22e85fa73636eeb032c40002b9
nevstruev-dmitriy <nevstruev.dmitriy@mail.ru>
2017-06-22 13:53:27 +0300

    Новый файл БеленкоАлена

--- TelegramBotLKSH/sempel/БеленкоАлена/БеленкоАлена.cpp
+++ TelegramBotLKSH/sempel/БеленкоАлена/БеленкоАлена.cpp
@@ -3,20 +3,22 @@
 using namespace std;
 
 int main() {
-	int turná cnt = 0;
+	int turn, cnt = 0;
 	while (true) {
 		int k = rand() % 2;
-		if (k == 1)
-		{
-			cnt++;
-		}
-		if (cnt == 3)
+		if (cnt >= 3)
 		{
 			cout << 3 << endl;
+			cnt -= 3;
 		}
 		else
 		{
 			cout << k << endl;
+		}
+
+		if (k == 1)
+		{
+			cnt++;
 		}
 		cout.flush();
 		cin >> turn;
