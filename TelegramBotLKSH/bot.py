2d96e143bffaf982175c0be1a0ecd7554133fed5
nevstruev-dmitriy <nevstruev.dmitriy@mail.ru>
2017-06-22 17:14:17 +0300

    all_testing

--- TelegramBotLKSH/bot.py
+++ TelegramBotLKSH/bot.py
@@ -308,16 +308,16 @@ def testing():
 				(str(ind), summa[i][1], summa[i][0])
 
 
-	out += "Cводная таблица:\n%s" % ("".center(3, "X"))
+	out += "Cводная таблица:\n%s" % ("".center(2, "X"))
 
 	for i in range(0, len(player)):
-		out += ("%d" % i).center(4, "_")
+		out += ("%d" % i).rjust(3, "_")
 	out += "\n"
 
 	for i in range(0, len(player)):
-		out += ("%d" % i).rjust(2, "_")
+		out += ("%d" % i).rjust(2, "0")
 		for j in range(0, len(player)):
-			out += str(ask[i][j]).center(4, "_")
+			out += "|" + str(ask[i][j]).rjust(2, "0")
 		out += "\n"
 	
 	out += "\n"
