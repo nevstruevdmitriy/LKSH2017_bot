84a980cb23fbb0d88434135103f10e3fe6a1a1de
nevstruev-dmitriy <nevstruev.dmitriy@mail.ru>
2017-06-22 11:48:57 +0300

    Новый файл НевструевДмитрий

--- TelegramBotLKSH/sempel/НевструевДмитрий/DimkaTask.cpp
+++ TelegramBotLKSH/sempel/НевструевДмитрий/DimkaTask.cpp
@@ -7,13 +7,17 @@ int main() {
 	srand(time(0));
 	cout << 0 << endl;
 	cout.flush();
+
+	int bal = 0;
 	
 	for (int i = 0; i < 100; ++i) {
-		if (rand() % 5 == 0) {
+		if (rand() % 5 == 0 && bal != 0) {
 			cout << 2 << endl;
+			bal = 0;
 			cout.flush();
 		} else if (rand() % 5 == 1) {
 			cout << 1 << endl;
+			++bal;
 			cout.flush();
 		} else {
 			cout << 0 << endl;
