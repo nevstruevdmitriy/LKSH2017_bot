3c19ce9749d62a186ccc0761efdb084f936e4701
nevstruev-dmitriy <nevstruev.dmitriy@mail.ru>
2017-06-24 12:46:50 +0300

    Новый файл ТерзиянГрант

---
+++ TelegramBotLKSH/sempel/ТерзиянГрант/ТерзиянГрант.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int turn;
+	cout << 1 << endl;
+    cout.flush();
+    cin >> turn;
+    cout << 0 << endl;
+    cout.flush();
+    cin >> turn;
+    while (true) {
+		cout << 2 << endl;
+	    cout.flush();
+	    cin >> turn;
+		cout << 1 << endl;
+		cout.flush();
+		cin >> turn;
+	}
+}
