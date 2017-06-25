0605d9770ac1fb41e2d0e4fc5a0afe30f33373fb
nevstruev-dmitriy <nevstruev.dmitriy@mail.ru>
2017-06-25 22:52:04 +0300

    Новый файл ТрильВсеволод

--- TelegramBotLKSH/sempel/ТрильВсеволод/ТрильВсеволод.cpp
+++ TelegramBotLKSH/sempel/ТрильВсеволод/ТрильВсеволод.cpp
@@ -9,42 +9,48 @@ int main()
 		{
 			if(cntm>=3)
 			{
-				cout<<3;
+				cout<<3<<endl;
+				cout.flush();
 				cntm=0;
 			}
 			else
 			{
-				cout<<2;	
+				cout<<2<<endl;
+				cout.flush();
+				cntm--;
 			}
 		}
 		else
 		{
 			if(cnto==0&&cntm>=1&&cntm<2)
 			{
-				cout<<1;
+				cout<<1<<endl;
+				cout.flush();
 				++cntm;
 			}
 			else
 			{
 				if(cntm>=3)
 				{
-					cout<<0;
+					cout<<0<<endl;
+					cout.flush();
 				}
 				else
 				{
-					if(cntm<=1&&cnto>=1)
+					if(cntm==0&&cnto>=1)
 					{
-						cout<<0;
+						cout<<0<<endl;
+						cout.flush();
 					}
 					else
 					{
-						cout<<1;
+						cout<<1<<endl;
+						cout.flush();
 						++cntm;
 					}
 				}
 			}
 		}
-		cout.flush();
 		cin>>turn;
 		if(turn==1)
 		{
