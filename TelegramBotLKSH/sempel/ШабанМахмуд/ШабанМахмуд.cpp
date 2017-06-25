9461860844412199f0670f2b3fb4fb16a1ad8faa
nevstruev-dmitriy <nevstruev.dmitriy@mail.ru>
2017-06-25 22:45:33 +0300

    Новый файл ШабанМахмуд

--- TelegramBotLKSH/sempel/ШабанМахмуд/ШабанМахмуд.cpp
+++ TelegramBotLKSH/sempel/ШабанМахмуд/ШабанМахмуд.cpp
@@ -6,13 +6,15 @@ int main() {
 	cout.flush();
 	i++;
 	while (cin >> k) {
-		if (i % 5 == 1)
+		if (i % 6 == 1)
 			cout << 0 << endl;
-		else if (i % 5 == 2)
+		else if (i % 6 == 2)
 			cout << 1 << endl;
-		else if (i % 3 != 5)
+		else if (i % 6 == 3)
+			cout << 0 << endl;
+		else if (i % 6 != 0)
 			cout << 2 << endl;
-		else
+		else 
 			cout << 1 << endl;
 		cout.flush();
 		i++;
