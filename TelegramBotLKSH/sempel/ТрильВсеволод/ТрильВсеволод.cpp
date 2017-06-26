053569d6b0cd127a077aa04f876a791d2a0b8911
nevstruev-dmitriy <nevstruev.dmitriy@mail.ru>
2017-06-26 22:07:21 +0300

    Новый файл ТрильВсеволод

--- TelegramBotLKSH/sempel/ТрильВсеволод/ТрильВсеволод.cpp
+++ TelegramBotLKSH/sempel/ТрильВсеволод/ТрильВсеволод.cpp
@@ -3,37 +3,65 @@ using namespace std;
 int main()
 {
 	int cnto=0, cntm=0, turn;
-	bool p=0;
-	while(true)
-	{
-		if(cntm<=1)
-		{
+	bool p1=0, p2=0, p3=0;
+	for(int i=0; true; ++i){
+		if(i==0){
 			cout<<1<<endl;
 			cout.flush();
-			cntm++;
 		}
-		else
-		{
-			if(cnto<=2)
+		else{
+			if(i%2==0)
 			{
 				cout<<0<<endl;
 				cout.flush();
 			}
-			if(cnto>2)
-			{
-				cout<<2<<endl;
-				cout.flush();
-				cntm=0;
-			}
-			
-		}
+			else{
+				if(i<=5)
+				{
+					if(i==3)
+					{
+						cout<<1<<endl;
+						cout.flush();
+					}
+					else
+					{
+						cout<<2<<endl;
+						cout.flush();
+					}
+				}
+				else
+				{
+					if(i<=13){
+						if(i==7||i==9){
+							cout<<1<<endl;
+							cout.flush();
+						}
+						else
+						{
+							cout<<2<<endl;
+							cout.flush();
+						}
+					}
+					else
+					{
+						if((i-13)%8==2||(i-13)%8==4||(i-13)%8==6){
+							cout<<1<<endl;
+							cout.flush();
+						}
+						else
+						{
+							cout<<3<<endl;
+							cout.flush();
+						}
+					}
+				}
+			}	
+		}	
 		cin>>turn;
-		if(turn>=2)
-		{
+		if(turn>=2){
 			cnto=0;
 		}
-		if(turn==1)
-		{
+		if(turn==1){
 			++cnto;
 		}
 	}
