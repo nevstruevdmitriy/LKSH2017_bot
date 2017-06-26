e106f6c14d8e95b23099599d88df5a127543fbc8
nevstruev-dmitriy <nevstruev.dmitriy@mail.ru>
2017-06-26 21:44:48 +0300

    Новый файл ЛободаАнтон

--- TelegramBotLKSH/sempel/ЛободаАнтон/ЛободаАнтон.cpp
+++ TelegramBotLKSH/sempel/ЛободаАнтон/ЛободаАнтон.cpp
@@ -1,29 +1,23 @@
 #include <iostream>
 using namespace std;
 
-void tern(int ans){
-      cout << ans << endl;
-      cout.flush();
+void tern(int n){
+	cout << n << endl;
+	cout.flush();
 }
 
-/*
-      0 çàùèòà
-      1 ïåðåçàðÿäêà
-      2 áåíã
-      3 ÁÅÍÃ ÁÅÍÃ ÁÅÍÃ
-*/
-
 int main() {
-      
-       int t;
-       tern(1);
-       tern(0);
-       tern(2);
-       tern(1);
-       tern(1);
-       tern(2);
-       tern(1);
-       tern(0);
-       tern(1);
-       tern(3);
+	
+	tern(1);
+	tern(0);
+	tern(0);
+	tern(2);
+	tern(1);
+	tern(1);
+	tern(0);
+	tern(1);
+	tern(3);
+	
+	
+	return 0;
 }
