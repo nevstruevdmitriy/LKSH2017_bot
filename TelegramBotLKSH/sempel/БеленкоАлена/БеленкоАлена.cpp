d1cc8658125ffb80d4eb52d9b72f8151571455c5
nevstruev-dmitriy <nevstruev.dmitriy@mail.ru>
2017-06-26 21:25:53 +0300

    Новый файл БеленкоАлена

--- TelegramBotLKSH/sempel/БеленкоАлена/БеленкоАлена.cpp
+++ TelegramBotLKSH/sempel/БеленкоАлена/БеленкоАлена.cpp
@@ -4,15 +4,13 @@ using namespace std;
 int a[114];
 int main() {
 	a[1] = 1;
-	a[2] = 2;
-	a[3] = 1;
+	a[2] = 1;
+	a[3] = 0;
 	a[4] = 1;
-	a[5] = 1;
-	a[6] = 3;
-	int i = 1;
+	a[5] = 3;
 	int turn, cnt = 0;
 	while (true) {
-		if (i <= 6)
+		if (i <= 5)
 		{
 			cout << a[i]<<endl;
 			i++;
