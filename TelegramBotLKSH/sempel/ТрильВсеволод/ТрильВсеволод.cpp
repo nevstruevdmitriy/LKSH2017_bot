539a7914788b9c424d6da0bd87a8fa060f32a170
nevstruev-dmitriy <nevstruev.dmitriy@mail.ru>
2017-06-26 22:16:21 +0300

    Новый файл ТрильВсеволод

--- TelegramBotLKSH/sempel/ТрильВсеволод/ТрильВсеволод.cpp
+++ TelegramBotLKSH/sempel/ТрильВсеволод/ТрильВсеволод.cpp
@@ -4,56 +4,31 @@ int main()
 {
 	int cnto=0, cntm=0, turn;
 	bool p1=0, p2=0, p3=0;
-	for(int i=0; true; ++i){
-		if(i==0){
+	for(int i=0; true; ++i)
+	{
+		if(i==0)
+		{
 			cout<<1<<endl;
 			cout.flush();
 		}
-		else{
+		else
+		{
 			if(i%2==0)
 			{
 				cout<<0<<endl;
 				cout.flush();
 			}
-			else{
-				if(i<=7)
+			else
+			{
+				if(i%7==3||i%7==5||i%7==1)
 				{
-					if(i==3||i==5)
-					{
-						cout<<1<<endl;
-						cout.flush();
-					}
-					else
-					{
-						cout<<2<<endl;
-						cout.flush();
-					}
+					cout<<1<<endl;
+					cout.flush();
 				}
 				else
 				{
-					if(i<=13){
-						if(i==7||i==9){
-							cout<<1<<endl;
-							cout.flush();
-						}
-						else
-						{
-							cout<<2<<endl;
-							cout.flush();
-						}
-					}
-					else
-					{
-						if((i-13)%8==2||(i-13)%8==4||(i-13)%8==6){
-							cout<<1<<endl;
-							cout.flush();
-						}
-						else
-						{
-							cout<<3<<endl;
-							cout.flush();
-						}
-					}
+					cout<<3<<endl;
+					cout.flush();
 				}
 			}	
 		}	
